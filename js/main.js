35ed0b148bfdbd73aa50c1ef421946361147a67b
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2020-12-05 22:33:30 +0200

    Baigtas

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ renderAllProgressBars(progressBarData);
 const toast = new Toast();
 toast.render();
 // toast.show('error', 'Cia yra klaida!!!');
-toast.show('success', 'Buvo gera diena!!! ;)');
+// toast.show('success', 'Buvo gera diena!!! ;)');
 
-formValidator('.hero .form');
-formValidator('main .form');
+formValidator('.hero .form', toast);
+formValidator('main .form', toast);
 
